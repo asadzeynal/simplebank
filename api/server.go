2dfd1b1b68d1017dbcf5b7ef603236ffb2e72a68
asadzeynal <asadzeynal9@gmail.com>
2023-01-18 18:17:26 +0400

    New routes added

--- api/server.go
+++ api/server.go
@@ -16,7 +16,9 @@ func NewServer(store *db.Store) *Server {
 	}
 	router := gin.Default()
 
-	router.POST("/account", server.createAccount)
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
 	server.router = router
 	return server
